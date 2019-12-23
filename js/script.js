8a1e0cbb5fc72742c5dd76a1e81c8e5e23698aa4
Nate Hodge <nhodge253@gmail.com>
2019-12-22 21:30:44 -0800

    initial commit. Set up home and about pages along with Sass and CSS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).ready(function () {
+    $(".view-details").click(function (a) {
+        var b = a.target,
+            c = $(b).parent().parent().children(".details");
+        $(c).is(":visible") === !0 ? ($(c).hide(), $(b).text("Backstory	»")) : ($(c).show(), $(b).text("Hide details »"))
+    })
+});
+
