4da241fc784de5accd8aa5184a570246be5f7bc9
Nate Hodge <nhodge253@gmail.com>
2020-01-02 23:10:56 -0800

    Moved slider and adjusted CSS accordingly

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
-$(document).ready(function () {
-    $(".view-details").click(function (a) {
-        var b = a.target,
-            c = $(b).parent().parent().children(".details");
-        $(c).is(":visible") === !0 ? ($(c).hide(), $(b).text("Backstory	»")) : ($(c).show(), $(b).text("Hide details »"))
-    })
+$(document).ready(function() {
+  $(".view-details").click(function(a) {
+    var b = a.target,
+      c = $(b)
+        .parent()
+        .parent()
+        .children(".details");
+    $(c).is(":visible") === !0 ? ($(c).hide(), $(b).text("Backstory	»")) : ($(c).show(), $(b).text("Hide details »"));
+  });
 });
-
